bd0474f61ce02e93ee18c224558798e1e57d552d
Braidon <63606230+Treefire33@users.noreply.github.com>
2022-04-27 16:23:19 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-alert("Game not ready! Feel free to leave!");
+var currentPeriod = 1;
+$(document).ready(function(){
+    $(".row").hide();
+    $("#p1").show();
+    $("#lbttn").click(function(){
+        if(currentPeriod !== 1)
+        {
+            currentPeriod -= 1;
+            $("#p"+currentPeriod.toString()).show();
+            $("#p"+(currentPeriod+1).toString()).hide();
+        }
+    });
+    $("#rbttn").click(function(){
+        if(currentPeriod !== 9)
+        {
+            currentPeriod += 1;
+            $("#p"+currentPeriod.toString()).show();
+            $("#p"+(currentPeriod-1).toString()).hide();
+        }
+    });
+});
