642a20c1f73ed5a9c271fa843afe3371dcfd1fc2
Braidon <63606230+Treefire33@users.noreply.github.com>
2022-04-29 14:40:52 -0500

    Create getElementFromSymbol.js
    only 4 elements so far.

---
+++ js/getElementFromSymbol.js
@@ -0,0 +1,19 @@
+function getElementFromSymbol(symbol)
+{
+  if(symbol === "h")
+  {
+    return "hydrogen";
+  }
+  if(symbol === "he")
+  {
+    return "helium";
+  }
+  if(symbol === "li")
+  {
+    return "lithium";
+  }
+  if(symbol === "be")
+  {
+    return "beryllium";
+  }
+}
