85d6283396b0b531b081e042ea8e7a17dc11e791
Braidon <63606230+Treefire33@users.noreply.github.com>
2022-05-02 11:42:12 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -32,14 +32,36 @@ $(document).ready(function(){
        var guessId = $(this).attr('id');
        guessQueued = guessId;
     });
+    var guesses = 1;
     $("#enter").click(function(){
         if(guessQueued !== "NAVA")
         {
             let newIMG = getElementFromSymbol(guessQueued);
             var val = newIMG, src = 'http://raw.githubusercontent.com/Treefire33/elementle/main/img/' + val +'.png', img = document.createElement('img');
             img.src = src;
-            $("#guesses").append(img, "<br>" ,newIMG, "<br>");
+            $("#guesses").append('<div id="guess'+guesses+'"'+'>', img, "<br>" ,newIMG, "</div>", "<br>");
+            var correctnessImage = checkCorrectness(newIMG);
+            var val = correctnessImage, src = 'http://raw.githubusercontent.com/Treefire33/elementle/main/img/' + val +'.png', img = document.createElement('img');
+            img.src = src;
+            $("#guess"+guesses).appended(img);
+            guesses = guesses + 1;
         }
         guessQueued = "NAVA";
     });
 });
+
+function checkCorrectness(answer)
+{
+    if(answer === correctAnswer)
+    {
+        return "check";
+    }
+    else if(answer === "he")
+    {
+        return "triangle";
+    }
+    else
+    {
+        return "cross";
+    }
+}
