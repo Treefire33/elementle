b5acdc3399b3f590e566c74237d1cf2bc9f63449
Braidon <63606230+Treefire33@users.noreply.github.com>
2022-05-04 13:48:36 -0500

    added rest of period 4

--- js/getElementFromSymbol.js
+++ js/getElementFromSymbol.js
@@ -76,13 +76,76 @@ function getElementFromSymbol(symbol)
   {
     return "argon";
   }
-  //Period 4
+  if(symbol === "k")
+  {
+    return "potassium";
+  }
+  if(symbol === "ca")
+  {
+    return "calcium";
+  }
+  if(symbol === "sc")
+  {
+    return "scandium";
+  }
+  if(symbol === "ti")
+  {
+    return "titanium";
+  }
+  if(symbol === "v")
+  {
+    return "vanadium";
+  }
+  if(symbol === "cr")
+  {
+    return "chromium";
+  }
+  if(symbol === "mn")
+  {
+    return "manganese";
+  }
   if(symbol === "fe")
   {
-    return "iron"
+    return "iron";
+  }
+  if(symbol === "co")
+  {
+    return "cobalt";
+  }
+  if(symbol === "ni")
+  {
+    return "nickel";
+  }
+  if(symbol === "cu")
+  {
+    return "copper";
+  }
+  if(symbol === "zn")
+  {
+    return "zinc";
+  }
+  if(symbol === "ga")
+  {
+    return "gallium";
+  }
+  if(symbol === "ge")
+  {
+    return "germanium";
+  }
+  if(symbol === "arsenic")
+  {
+    return "arsenic";
   }
   if(symbol === "se")
   {
     return "selenium";
+  }
+  if(symbol === "br")
+  {
+    return "bromine";
+  }
+  if(symbol === "kr")
+  {
+    return "krypton";
   }
 }
